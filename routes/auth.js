48bdb0287fead26f1ee818ec80f38a6266bf65a3
Marie Garde <marie_garde@ymail.com>
2021-02-02 15:49:52 +0100

    more fix

--- routes/auth.js
+++ routes/auth.js
@@ -7,23 +7,23 @@ const jwt = require("jsonwebtoken");
 require("../passport-strategies");
 
 router.post("/signup", (req, res) => {
-  const formData = req.body;
-  formData.password = bcrypt.hashSync(formData.password, jwt_rounds);
-  db.query(`INSERT INTO Client SET ?`, formData, (errSql, resSql) => {
-    if (!errSql) {
-      delete formData.password;
-      formData.id = resSql.insertId;
-      const token = jwt.sign(formData, jwt_secret);
-      res.status(201).json(token);
-    } else {
-      res.status(500).json(errSql);
-    }
-  });
+    const formData = req.body;
+    formData.password = bcrypt.hashSync(formData.password, jwt_rounds);
+    db.query(`INSERT INTO client SET ?`, formData, (errSql, resSql) => {
+        if (!errSql) {
+            delete formData.password;
+            formData.id = resSql.insertId;
+            const token = jwt.sign(formData, jwt_secret);
+            res.status(201).json(token);
+        } else {
+            res.status(500).json(errSql);
+        }
+    });
 });
 
 router.post("/login", passport.authenticate("local"), (req, res) => {
-  const token = jwt.sign(req.user, jwt_secret);
-  res.status(200).json(token);
+    const token = jwt.sign(req.user, jwt_secret);
+    res.status(200).json(token);
 });
 
 module.exports = router;
--- routes/misc.js
+++ routes/misc.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
 
 //Get all clients
 router.get("/client", (req, res) => {
-    db.query("SELECT * from Client", (err, results) => {
+    db.query("SELECT * from client", (err, results) => {
         if (err) {
             res.status(500).send("Error retrieving data");
         } else {
@@ -23,7 +23,7 @@ router.get("/client", (req, res) => {
 router.delete("/client/:id", (req, res) => {
     const idClient = req.params.id;
     db.query(
-        "DELETE FROM Client WHERE idClient = ?", [idClient],
+        "DELETE FROM client WHERE idClient = ?", [idClient],
         (err, results) => {
             if (err) {
                 res.status(500).send("😱 Error deleting an client");
@@ -42,7 +42,7 @@ router.patch("/client/:id", (req, res) => {
     const newClient = req.body;
     // We send a UPDATE query to the DB
     db.query(
-        "UPDATE Client SET ? WHERE idClient = ?", [newClient, idClient],
+        "UPDATE client SET ? WHERE idClient = ?", [newClient, idClient],
         (err, results) => {
             if (err) {
                 res.status(500).send("Error updating an client");
@@ -57,12 +57,94 @@ router.patch("/client/:id", (req, res) => {
 router.get("/client/:id", (req, res) => {
     const idClient = req.params.id;
     db.query(
-        "SELECT idClient, username, surname, email, phone, job, structure_name, structure_type, structure_field, job_field, territory, wishes, website FROM Client WHERE idClient = ?", [idClient],
+        "SELECT idClient, username, surname, email, phone, job, structure_name, structure_type, structure_field, job_field, territory, wishes, website FROM client WHERE idClient = ?", [idClient],
         (err, results) => {
             if (err) {
                 res.status(500).send("Error client");
             } else {
                 res.status(200).json(results);
+            }
+        }
+    );
+});
+
+//Get all Ressources Favart
+router.get("/ressources", (req, res) => {
+    db.query("SELECT * from ressources_favart", (err, results) => {
+        if (err) {
+            res.status(500).send("Error retrieving data");
+        } else {
+            res.status(200).json(results);
+        }
+    });
+});
+
+//Post a new Favart doc
+router.post("/ressources", (req, res) => {
+    const { link, title, description } = req.body;
+    db.query(
+        "INSERT INTO ressources_favart(link, title, description) VALUES(?, ?, ?)", [link, title, description],
+        (err, response) => {
+            if (err) {
+                res.status(500).send("Error saving a doc");
+            } else {
+                res.status(200).send("Successfully saved !");
+            }
+        }
+    );
+});
+
+//Delete a Favart doc
+router.delete("/ressources/:id", (req, res) => {
+    const idClient = req.params.id;
+    db.query(
+        "DELETE FROM ressources_favart WHERE idRessources_Favart = ?", [idClient],
+        (err, results) => {
+            if (err) {
+                res.status(500).send("😱 Error deleting a doc");
+            } else {
+                res.status(200).send("🎉 Doc deleted!");
+            }
+        }
+    );
+});
+
+//Get all Ressources Ext
+router.get("/ressourcesext", (req, res) => {
+    db.query("SELECT * from ressources_externes", (err, results) => {
+        if (err) {
+            res.status(500).send("Error retrieving data");
+        } else {
+            res.status(200).json(results);
+        }
+    });
+});
+
+//Post a new ext doc
+router.post("/ressourcesext", (req, res) => {
+    const { link, title, description } = req.body;
+    db.query(
+        "INSERT INTO ressources_externes(link, title, description) VALUES(?, ?, ?)", [link, title, description],
+        (err, response) => {
+            if (err) {
+                res.status(500).send("Error saving a doc");
+            } else {
+                res.status(200).send("Successfully saved !");
+            }
+        }
+    );
+});
+
+//Delete a ext doc
+router.delete("/ressourcesext/:id", (req, res) => {
+    const idClient = req.params.id;
+    db.query(
+        "DELETE FROM ressources_externes WHERE idRessources_Externes = ?", [idClient],
+        (err, results) => {
+            if (err) {
+                res.status(500).send("😱 Error deleting a doc");
+            } else {
+                res.status(200).send("🎉 Doc deleted!");
             }
         }
     );
